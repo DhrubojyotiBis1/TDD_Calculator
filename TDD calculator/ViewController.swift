e33c527ea5f003d2fcc48cf7e2c47bd0e08a18a6
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-03-22 02:30:18 +0530

    main logic implementation complete

--- TDD calculator/ViewController.swift
+++ TDD calculator/ViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLable:UILabel!
+    var result:String!
+    var operrationTag:Int!
+    var firstvalue:Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.setup()
     }
+    
+    @IBAction func actionPressed(_ sender:UIButton){
+        self.resultLable.text = result + String(sender.tag)
+        self.result = self.resultLable.text!
+    }
+    @IBAction func operrationPressed(_ sender:UIButton){
+        guard (result != "") else {
+            return
+        }
+        if operrationTag == nil{
+            self.firstvalue = Int(self.resultLable.text!)
+        }
+        result = ""
+        self.operrationTag = sender.tag
+        if sender.tag == -1{
+            //clear all
+            firstvalue = 0
+            self.resultLable.text = "0"
+            self.operrationTag = nil
+        }
+    }
+    
+    @IBAction func equalButtonPressed(_ sender:UIButton){
+        guard (result != "") else {
+            return
+        }
+        let secondNumber = Int(result)!
+        if operrationTag == 1{
+            //add
+            self.result = String(calculate().addNumber(X: self.firstvalue, Y: secondNumber))
+        }else if operrationTag == 0{
+            //sub
+            self.result = String(calculate().subNumber(X: self.firstvalue, Y: secondNumber))
+        }
+        self.resultLable.text = self.result
+        self.firstvalue = Int(result)
+    }
+}
 
-
+extension ViewController{
+    //All private function
+    private func setup(){
+        self.firstvalue = 0
+        self.result = ""
+    }
 }
 
