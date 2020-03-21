e8b7e5c1a85b627b1a2d7f9076cd640b0e559638
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-03-22 01:04:02 +0530

    Model and test complete

---
+++ TDD calculator/model/calculate.swift
@@ -0,0 +1,19 @@
+//
+//  calculate.swift
+//  TDD calculator
+//
+//  Created by Dhrubojyoti on 22/03/20.
+//  Copyright © 2020 Dhrubojyoti. All rights reserved.
+//
+
+import Foundation
+
+class calculate {
+    func addNumber(X:Int,Y:Int)->Int{
+        return X+Y
+    }
+    
+    func subNumber(X:Int,Y:Int)->Int{
+        return X-Y
+    }
+}
--- TDD calculatorTests/TDD_calculatorTests.swift
+++ TDD calculatorTests/TDD_calculatorTests.swift
@@ -11,24 +11,16 @@ import XCTest
 
 class TDD_calculatorTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let calculateBrain = calculate()
+    
+    func testForAddition(){
+        let sum = self.calculateBrain.addNumber(X:10,Y:10)
+        XCTAssertEqual(20, sum)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testForSub(){
+        let diffference = self.calculateBrain.subNumber(X:10,Y:10)
+        XCTAssertEqual(0, diffference)
     }
 
 }
